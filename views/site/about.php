5c95fe9c05f19f05c17c07f860d16161215a14fb
Sergeitsev <Sergeitsev@gmail.com>
2016-03-26 13:51:53 +0200

    init commit

---
+++ views/site/about.php
@@ -0,0 +1,26 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+/*$this->title = 'About';*/
+/*$this->params['breadcrumbs'][] = $this->title;*/
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+    <div id="wrapper">
+        <div id="hero">
+            <div class="header">
+                <h1 style="font-size:300%;"> About Seeker </h1>
+            </div>
+        </div>
+        <div id="content">
+            <h2> Know yourself.</h2>
+            <p>  The maxim, or aphorism, "know thyself" has had a variety of meanings attributed to it in literature. The Suda, a 10th-century encyclopedia of Greek knowledge, says: "the proverb is applied to those whose boasts exceed what they are", and that "know thyself" is a warning to pay no attention to the opinion of the multitude.
+
+                In Latin the aphorism is generally given as nosce te ipsum or temet noscet wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. </p>
+        </div>
+
+
+</div>
